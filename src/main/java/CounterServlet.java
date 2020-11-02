1b3738f246e534c73a76f282d46b791b39a2fd59
Roberto <roberto.cuyar44@gmail.com>
2020-11-02 16:37:15 -0600

    Separated weblets for better functionality overall

--- src/main/java/CounterServlet.java
+++ src/main/java/CounterServlet.java
@@ -1,3 +1,4 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -5,21 +6,22 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet (name = "HelloWorld Servlet", urlPatterns = "/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet (name = "Counter Servlet", urlPatterns = "/count")
+public class CounterServlet extends HttpServlet {
+    int counter = 1;
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         res.setContentType("text/html");
         PrintWriter out = res.getWriter();
-        String name = req.getParameter("name");
-        if(name != null){
-            name = name.substring(0,1).toUpperCase() + name.substring(1);
-            out.println("<h1> Hello " + name + "!</h1>");
+        String confirm = req.getParameter("reset");
+        if (confirm != null) {
+            counter = 1;
+            out.println("<h1>Counter reset</h1>");
         } else {
-            out.println("<h1> Hello World!</h1>");
+            out.printf("<h1>%d</h1>", counter);
+            counter++;
         }
+
+
     }
 }
-
-
-
