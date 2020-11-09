6685e8d06bafc75918ee02d55367955bf88863df
Roberto <roberto.cuyar44@gmail.com>
2020-11-09 11:39:03 -0600

    Part 1 of user-input exercise attempted

--- src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
+++ src/main/java/com/codeup/adlister/dao/MySQLAdsDao.java
@@ -1,14 +1,13 @@
 package com.codeup.adlister.dao;
 
+import com.codeup.adlister.Config;
 import com.codeup.adlister.models.Ad;
 import com.mysql.cj.jdbc.Driver;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class MySQLAdsDao implements Ads {
     private Connection connection = null;
@@ -41,8 +40,10 @@ public class MySQLAdsDao implements Ads {
     @Override
     public Long insert(Ad ad) {
         try {
-            Statement stmt = connection.createStatement();
-            stmt.executeUpdate(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(ad), Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, ad.getTitle());
+            stmt.setString(2, ad.getDescription());
+            stmt.executeUpdate();
             ResultSet rs = stmt.getGeneratedKeys();
             rs.next();
             return rs.getLong(1);
@@ -52,10 +53,9 @@ public class MySQLAdsDao implements Ads {
     }
 
     private String createInsertQuery(Ad ad) {
-        return "INSERT INTO ads(user_id, title, description) VALUES "
-            + "(" + ad.getUserId() + ", "
-            + "'" + ad.getTitle() +"', "
-            + "'" + ad.getDescription() + "')";
+       return "INSERT INTO ads(user_id, title, description) VALUES "
+            + "(" + ad.getUserId() + ", ?, ?)";
+
     }
 
     private Ad extractAd(ResultSet rs) throws SQLException {
---
+++ src/main/java/com/codeup/adlister/dao/MySQLUsersDao.java
@@ -0,0 +1,62 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.Config;
+import com.mysql.cj.jdbc.Driver;
+import com.codeup.adlister.models.User;
+
+import java.sql.*;
+
+public class MySQLUsersDao implements Users {
+    private Connection connection = null;
+
+    public MySQLUsersDao (Config config){
+    try {
+        DriverManager.registerDriver(new Driver());
+        connection = DriverManager.getConnection(
+                config.getUrl(),
+                config.getUser(),
+                config.getPassword()
+        );
+    } catch (
+    SQLException e) {
+        throw new RuntimeException("Error connecting to the database!", e);
+    }
+}
+@Override
+    public User findByUsername (String username){
+        String query = "SELECT * FROM users WHERE username = ?";
+        String usernameSearch = "%" + username + "%";
+
+        try {
+            PreparedStatement stmt = connection.prepareStatement(query);
+            stmt.setString(1, usernameSearch);
+            ResultSet rs = stmt.executeQuery();
+            rs.next();
+           return new User(rs.getLong("id"), rs.getString("username"),rs.getString("email"), rs.getString("password"));
+
+
+        } catch (SQLException e){
+            throw new RuntimeException("Error finding user.");
+        }
+
+}
+@Override
+    public Long insert(User user){
+        try {
+            PreparedStatement stmt = connection.prepareStatement(createInsertQuery(user), Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUsername());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, user.getPassword());
+            stmt.executeUpdate();
+            ResultSet rs = stmt.getGeneratedKeys();
+            rs.next();
+            return rs.getLong(1);
+        } catch (SQLException e){
+            throw new RuntimeException("Error creating a new user.");
+        }
+}
+
+private  String createInsertQuery(User user){
+        return "INSERT INTO users( username, email, password) VALUES(?, ?, ?)";
+}
+}
